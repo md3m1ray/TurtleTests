bd4ee30a2ade1448d9ee4fd8130107f099ff3e1d
Muhammet Demiray <md3m1ray@gmail.com>
2023-12-16 21:47:51 +0300

    Star Drawing

---
+++ main.py
@@ -0,0 +1,18 @@
+import turtle
+
+drawing_board = turtle.Screen()
+drawing_board.bgcolor("light blue")
+drawing_board.title("Turtle Test")
+
+turtle_instance = turtle.Turtle()
+
+""" Star Drawing
+for i in range(5):
+
+    turtle_instance.left(144)
+    turtle_instance.forward(200)
+"""
+
+
+
+turtle.done()
